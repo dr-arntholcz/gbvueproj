95b85991f9bb70003a2f90c0c58a2f5b8987b950
Stanislav <dr.arntholcz@mail.ru>
2021-05-05 13:49:18 +0700

    dgdfgf

--- src/components/calculator.vue
+++ src/components/calculator.vue
@@ -95,7 +95,7 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only //////////-->
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .calc {
   margin: 0 calc(50% - 150px) 0;
